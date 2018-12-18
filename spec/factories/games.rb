f818ed9175fa2dfe9913bed61bc4a95f6791be2d
Mesky <dmeskis@gmail.com>
2018-12-18 11:43:58 -0700

    Creates game and tests attributes/validations

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :game do
-    total_duration { 1 }
-    level_one_duration { 1 }
-    level_two_duration { 1 }
-    level_three_duration { 1 }
-    level_four_duration { 1 }
-    remaining_life { 1 }
+    total_duration { Faker::Number.number(5) }
+    level_one_duration { Faker::Number.number(5) }
+    level_two_duration { Faker::Number.number(5) }
+    level_three_duration { Faker::Number.number(5) }
+    level_four_duration { Faker::Number.number(5) }
+    remaining_life { Faker::Number.between(0, 3) }
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'exists' do
+    game = create(:game)
+    Game.where(total_duration: game.total_duration).should exist
+  end
+  describe 'validations' do
+    it { should validate_presence_of :total_duration }
+    it { should validate_presence_of :remaining_life }
+  end
 end
