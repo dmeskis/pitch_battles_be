72247334b6fb4555ef3345c9024d3f649c2847dd
Mesky <dmeskis@gmail.com>
2018-12-18 07:53:16 -0700

    Configure user migration file, set limits on strings and default values

--- db/migrate/20181218145000_create_users.rb
+++ db/migrate/20181218145000_create_users.rb
@@ -2,11 +2,11 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :email
-      t.string :first_name
-      t.string :last_name
-      t.integer :role
+      t.string :first_name, limit: 32
+      t.string :last_name, limit: 32
+      t.integer :role, default: 0
       t.string :password_digest
-      t.integer :avatar
+      t.integer :avatar, default: 1
 
       t.timestamps
     end
