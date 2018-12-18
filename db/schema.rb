f605a5413e4d52b6bc8d0f817ab453b8851e0255
Mesky <dmeskis@gmail.com>
2018-12-18 11:15:22 -0700

    Ran migration for UserKlasses join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_18_174045) do
+ActiveRecord::Schema.define(version: 2018_12_18_181025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 2018_12_18_174045) do
     t.string "class_key"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "klasses_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "klass_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
