3e8894eedbe6962a6ac571057a48c5b9190b3d93
Mesky <dmeskis@gmail.com>
2018-12-18 08:29:17 -0700

    Added validations for user, unique email, secure password, all tests passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,6 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :last_name}
     it { should validate_presence_of :password}
     it { should validate_presence_of :role}
+    it { should validate_uniqueness_of :email}
   end
 end
