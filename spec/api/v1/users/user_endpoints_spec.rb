8d50575e5a96881fe4fd1c4f66fe9f22dd98c854
Mesky <dmeskis@gmail.com>
2018-12-18 12:58:53 -0700

    Wrote test for user creation endpoint

---
+++ spec/api/v1/users/user_endpoints_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+describe 'user api', :type => :request do
+  it 'can create a user' do
+    headers = { 'CONTENT_TYPE' => 'application/json',
+                'ACCEPT' => 'application/json'
+              }
+
+    body = {
+            email: "example@mail.com",
+            first_name: "billy",
+            last_name: "bob",
+            role: 0,
+            password: "password",
+            password_confirmation: "password"
+    }
+
+    post "/api/v1/users", :params => body
+
+    expect(response).to be_successful
+    expect(User.count).to eq(1)
+    user = User.first
+    expect(user.first_name).to eq("billy")
+    expect(user.last_name).to eq("bob")
+    expect(user.role).to eq("0")
+  end
+end
+
+# post /api/v1/users
+
+# As a user,
+# I send the following request:
+
+# {
+# email: “example@mail.com”,
+# first_name: “billy”,
+# last_name: “bob”,
+# role: 0,
+# password: “password”,
+# password_confirmation: “password”
+# }
+
+# All fields are required.
+# If post is successful, returns the following response with status 200:
+# Response
+# {
+# “success”: “Account successfully created”
+# }
+
+# If post fails, returns the following response with a 400 error:
+# {
+# "error": "Account creation failed. Please try again."
+# }
