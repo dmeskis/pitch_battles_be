a4a9f387417fb5d6db37586a53b4ca109f06d518
Mesky <dmeskis@gmail.com>
2018-12-18 11:06:24 -0700

    Tests klass model thoroughly, adds a create class key that runs before saving a class

--- app/models/klass.rb
+++ app/models/klass.rb
@@ -1,2 +1,14 @@
 class Klass < ApplicationRecord
+  validates_presence_of :name
+  validates_uniqueness_of :class_key
+  before_save :create_class_key
+
+  private
+
+    def create_class_key
+      loop do
+        class_key = self.class_key = SecureRandom.urlsafe_base64
+        break class_key unless self.class.exists?(class_key: class_key)
+      end
+    end
 end
--- spec/factories/klasses.rb
+++ spec/factories/klasses.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :klass do
-    name { "MyString" }
-    class_key { "MyString" }
+    name { "Test Class" }
   end
 end
