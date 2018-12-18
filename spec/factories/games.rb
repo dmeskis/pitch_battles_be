e6f3641e13508f0c6088c947d0f5721175a383bf
Mesky <dmeskis@gmail.com>
2018-12-18 11:37:45 -0700

    Generated game model

---
+++ spec/factories/games.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :game do
+    total_duration { 1 }
+    level_one_duration { 1 }
+    level_two_duration { 1 }
+    level_three_duration { 1 }
+    level_four_duration { 1 }
+    remaining_life { 1 }
+  end
+end
