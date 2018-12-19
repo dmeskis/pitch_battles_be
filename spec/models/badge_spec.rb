984a6307b0d7219cc869d52530d6010db1f50638
Mesky <dmeskis@gmail.com>
2018-12-19 13:26:29 -0700

    Migrated badge

--- spec/models/badge_spec.rb
+++ spec/models/badge_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Badge, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'exists' do
+    badge = create(:badge)
+    Badge.where(name: badge.name).should exist
+  end
+  
+  describe 'validations' do
+    it { should validate_presence_of :name}
+    it { should validate_presence_of :description}
+  end
 end
