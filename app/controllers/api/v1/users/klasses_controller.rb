48017720cfbcfc4b4ce87e9b71022f5f8fc70d68
Mesky <dmeskis@gmail.com>
2018-12-19 12:04:01 -0700

    Test sad path for adding a user to a class

--- app/controllers/api/v1/users/klasses_controller.rb
+++ app/controllers/api/v1/users/klasses_controller.rb
@@ -3,7 +3,13 @@ class Api::V1::Users::KlassesController < ApplicationController
   def create
     user = User.where(id: klass_params[:id])
     klass = Klass.find_by(class_key: klass_params[:class_key])
-    binding.pry
+    if user && klass
+      if klass.users << user
+        render json: {"success": "Successfully joined #{klass.name}!"}, status: 200
+      end
+    else
+      render json: {"error": "Incorrect credentials."}, status: 404
+    end
   end
 
   private
--- spec/api/v1/klasses/class_endpoints_spec.rb
+++ spec/api/v1/klasses/class_endpoints_spec.rb
@@ -5,7 +5,6 @@ describe 'game api', :type => :request do
     it 'can add a user to a class' do
       user = create(:user)
       klass = create(:klass)
-      binding.pry
       body = {
         class_key: klass.class_key
       }
@@ -15,7 +14,22 @@ describe 'game api', :type => :request do
       parsed = JSON.parse(response.body)
       expect(response).to be_successful
       expect(response.status).to eq(200)
-      expect(user.klasses.count).to eq(1)
+      expect(klass.users.count).to eq(1)
+      expect(klass.users.first.first_name).to eq(user.first_name)
+    end
+    it 'errors if incorrect credentials' do
+      user = create(:user)
+      klass = create(:klass)
+      body = {
+        class_key: "N07 4 R341 |<3Y"
+      }
+
+      post "/api/v1/users/#{user.id}/class", :params => body
+
+      parsed = JSON.parse(response.body)
+      expect(response.status).to eq(404)
+      expect(parsed["error"]).to eq("Incorrect credentials.")
+      expect(klass.users.count).to eq(0)
     end
   end
 end
