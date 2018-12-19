8067b086c4b97035890e79ab06771ab1c78d9667
Mesky <dmeskis@gmail.com>
2018-12-19 11:13:00 -0700

    Customized user games serializer to display games under attributes

--- app/serializers/user_games_serializer.rb
+++ app/serializers/user_games_serializer.rb
@@ -1,6 +1,7 @@
 class UserGamesSerializer
   include FastJsonapi::ObjectSerializer
   attributes :email, :first_name, :last_name
+  set_type :user
   attribute :games do |obj|
     GameSerializer.new(Game.where(user_id: obj.id))
   end
