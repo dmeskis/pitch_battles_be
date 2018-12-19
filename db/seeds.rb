810d5b00e1617cd4487e4c205574d4d9bc989cbe
Mesky <dmeskis@gmail.com>
2018-12-19 13:51:08 -0700

    Creates seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'factory_bot_rails'
+require 'database_cleaner'
+
+# Clean database everytime you seed
+DatabaseCleaner.strategy = :truncation
+DatabaseCleaner.clean
+
+# Create badges
+10.times do
+  create(:badge)
+end
+
+# Create games and users
+# Factory bot creates a user associated with a game
+10.times do
+  create(:game)
+end
+
+# Create a class
+class = create(:klass)
+
+# Add users to class
+User.all.each do |user|
+  class.users << user
+end
+
+# Give each user a random badge
+badges = Badge.all
+User.all.each do |user|
+  user.badges << badges.sample
+end
