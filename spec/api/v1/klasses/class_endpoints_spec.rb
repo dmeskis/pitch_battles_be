1c0d6d462b592264f6fe8399f35495883c077171
Mesky <dmeskis@gmail.com>
2018-12-19 11:34:46 -0700

    Wrote test for adding user to class

---
+++ spec/api/v1/klasses/class_endpoints_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'game api', :type => :request do
+  describe 'post' do
+    it 'can add a user to a class' do
+      user = create(:user)
+      klass = create(:klass)
+      body = {
+        class_key: klass.class_key
+      }
+
+      post "/api/v1/users/#{user.id}/class", :params => body
+
+      parsed = JSON.parse(response.body)
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      expect(user.klasses.count).to eq(1)
+    end
+  end
+end
