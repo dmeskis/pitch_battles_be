3f543d8547c2bffa0a3e650cb395bed37f40c7e7
Mesky <dmeskis@gmail.com>
2018-12-19 20:24:58 -0700

    ran migation for user auth token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_19_203418) do
+ActiveRecord::Schema.define(version: 2018_12_20_032339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2018_12_19_203418) do
     t.integer "avatar", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "auth_token"
   end
 
   add_foreign_key "games", "users"
