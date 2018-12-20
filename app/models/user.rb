6f693f619533895dc09252d3d57ec06843cbf4d2
Mesky <dmeskis@gmail.com>
2018-12-19 20:31:34 -0700

    Wrote method for creating user auth token on create

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,20 @@ class User < ApplicationRecord
   validates_presence_of :first_name,
                         :last_name,
                         :role
+  before_save :generate_auth_token
   has_many :user_klasses
   has_many :klasses, through: :user_klasses
   has_many :games
   has_many :user_badges
   has_many :badges, through: :user_badges
+
+  private 
+
+  def generate_auth_token
+    loop do
+      token = self.auth_token = SecureRandom.urlsafe_base64
+      break token unless self.class.exists?(auth_token: auth_token)
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,7 +26,6 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :last_name }
     it { should validate_presence_of :password }
     it { should validate_presence_of :role }
-    it { should validate_presence_of :auth_token }
     it { should validate_uniqueness_of :email }
   end
   describe 'relationships' do
