557363e9c99371aed2bf82d97140e46fe567ed10
Mesky <dmeskis@gmail.com>
2018-12-23 14:26:43 -0600

    Test user show displays game and badges

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,10 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :email, :first_name, :last_name
+  attribute :games do |obj|
+    GameSerializer.new(Game.where(user_id: obj.id))
+  end
+  attribute :badges do |obj|
+    BadgeSerializer.new(obj.badges)
+  end
 end
