848639c582564cead89178ea576d829942dbe3a4
Mesky <dmeskis@gmail.com>
2018-12-24 15:04:17 -0600

    Changed migration to add users to klasses instead of games

--- db/migrate/20181224210221_add_user_to_games.rb
+++
@@ -1,5 +0,0 @@
-class AddUserToGames < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :games, :user, foreign_key: true
-  end
-end
---
+++ db/migrate/20181224210354_add_user_to_klasses.rb
@@ -0,0 +1,5 @@
+class AddUserToKlasses < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :klasses, :user, foreign_key: true
+  end
+end
