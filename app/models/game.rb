cb1057c91326219e82678000bfcd7f4464381835
Mesky <dmeskis@gmail.com>
2018-12-24 15:08:32 -0600

    Corrected migrations, add reference in game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,7 @@ class Game < ApplicationRecord
   validates_presence_of :total_duration,
                         :remaining_life
   belongs_to :user
+  belongs_to :teacher, class_name: "User"
   has_many :klass_games
   has_many :klasses, through: :klass_games
 end
--- db/migrate/20181224210354_add_user_to_klasses.rb
+++ db/migrate/20181224210354_add_user_to_klasses.rb
@@ -1,5 +1,6 @@
 class AddUserToKlasses < ActiveRecord::Migration[5.2]
   def change
-    add_reference :klasses, :user, foreign_key: true
+    add_reference :klasses, :teacher, index: true
+    add_foreign_key :klasses, :users, column: :teacher_id
   end
 end
