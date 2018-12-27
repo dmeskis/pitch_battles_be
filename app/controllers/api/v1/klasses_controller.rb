2ffb4fed166121c352991d1354b4cf895bffa586
Mesky <dmeskis@gmail.com>
2018-12-27 13:34:01 -0600

    Pull out conditionals in KlassesController to private methods

--- app/controllers/api/v1/klasses_controller.rb
+++ app/controllers/api/v1/klasses_controller.rb
@@ -1,30 +1,23 @@
 class Api::V1::KlassesController < ApplicationController
+  before_action :set_variables, only: :destroy
+  before_action :set_create_variables, only: :create
+  before_action :is_teacher?, only: :create
+  before_action :is_class_teacher?, only: :destroy
   before_action :validate_name_present, only: :create
   
   def create
-    if @current_user.teacher?
-      klass = Klass.new(klass_params)
-      klass.teacher_id = @current_user.id
-      if klass.save
-        render json: KlassSerializer.new(klass).serialized_json, status: 200
-      else
-        render json: {"error": "Unable to save class, please try again."}, status: 500
-      end
+    if @klass.save
+      render json: KlassSerializer.new(@klass).serialized_json, status: 200
     else
-      render json: {"error": "You must be a teacher to create a class"}, status: 401
+      render json: {"error": "Unable to save class, please try again."}, status: 500
     end
   end
 
   def destroy
-    klass = Klass.where(id: params[:id]).first
-    if klass.teacher == @current_user
-      if klass.delete
-        render json: {"success": "Successfully deleted #{klass.name}."}
-      else
-        render json: {"error": "Unable to delete #{klass.name}, please try again."}
-      end
+    if @klass.delete
+      render json: {"success": "Successfully deleted #{@klass.name}."}
     else
-      render json: {"error": "You must be a teacher of a class to delete it."}, status: 401
+      render json: {"error": "Unable to delete #{@klass.name}, please try again."}
     end
   end
 
@@ -32,6 +25,27 @@ class Api::V1::KlassesController < ApplicationController
 
   def klass_params
     params.permit(:name)
+  end
+
+  def set_variables
+    @klass = Klass.where(id: params[:id]).first
+  end
+
+  def set_create_variables
+    @klass = Klass.new(klass_params)
+    @klass.teacher_id = @current_user.id
+  end
+
+  def is_teacher?
+    unless @current_user.teacher?
+      render json: {"error": "You must be a teacher to create a class"}, status: 401
+    end
+  end
+
+  def is_class_teacher?
+    unless @klass.teacher == @current_user
+      render json: {"error": "You must be a teacher of a class to delete it."}, status: 401
+    end
   end
 
   def validate_name_present
