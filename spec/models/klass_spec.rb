daf516d0d803ce06ee663eddc816f3678f1c2215
Mesky <dmeskis@gmail.com>
2018-12-27 13:19:52 -0600

    adds validation of teacher for klass

--- spec/models/klass_spec.rb
+++ spec/models/klass_spec.rb
@@ -17,6 +17,7 @@ RSpec.describe Klass, type: :model do
   end
   describe 'validations' do
     it { should validate_presence_of :name }
+    it { should validate_presence_of :teacher_id }
     it { should validate_uniqueness_of :class_key}
   end
   describe 'relationships' do
