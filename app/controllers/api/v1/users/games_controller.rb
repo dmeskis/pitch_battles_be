8fe14a91f93907e453e8bfe6a5157a5940d8ef24
Mesky <dmeskis@gmail.com>
2018-12-27 13:47:21 -0600

    Pulls out setting variables in Users::GamesController

--- app/controllers/api/v1/users/games_controller.rb
+++ app/controllers/api/v1/users/games_controller.rb
@@ -1,11 +1,18 @@
 class Api::V1::Users::GamesController < ApplicationController
-  
+  before_action :set_variables
+
   def index
-    user = User.where(id: params[:id]).first
-    if user
-      render json: UserGamesSerializer.new(user).serialized_json, status: 200
+    if @user
+      render json: UserGamesSerializer.new(@user).serialized_json, status: 200
     else
       render json: {"error": "User not found."}, status: 404
     end
   end
+
+  private
+
+  def set_variables
+    @user = User.where(id: params[:id]).first
+  end
+
 end
