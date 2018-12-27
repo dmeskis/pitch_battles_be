df38ad33158ca2be1dc005eb30f7fa6cd96b0136
Mesky <dmeskis@gmail.com>
2018-12-27 14:33:27 -0600

    Migrated password columns to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_24_210354) do
+ActiveRecord::Schema.define(version: 2018_12_27_203056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,8 @@ ActiveRecord::Schema.define(version: 2018_12_24_210354) do
     t.integer "avatar", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
   end
 
   add_foreign_key "games", "users"
