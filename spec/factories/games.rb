4f0ccbe6e811745f337583d460605b4da1d7fb04
Mesky <dmeskis@gmail.com>
2018-12-29 12:34:30 -0700

    Removed remaining_life from game model and spec

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -5,7 +5,6 @@ FactoryBot.define do
     level_two_duration { Faker::Number.number(5) }
     level_three_duration { Faker::Number.number(5) }
     level_four_duration { Faker::Number.number(5) }
-    remaining_life { Faker::Number.between(0, 3) }
     user
   end
 end
