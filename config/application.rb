532aa866760999d8cf2ba185a56e46d969279f61
Mesky <dmeskis@gmail.com>
2018-12-29 14:36:28 -0700

    Allow patche request

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module PitchBattlesBe
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :patch, :options]
       end
     end
 
