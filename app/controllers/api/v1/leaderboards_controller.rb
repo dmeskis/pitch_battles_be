cf4c534a1c8037b17ed975ca5f8e20eafd778746
Mesky <dmeskis@gmail.com>
2018-12-31 16:55:24 -0700

    BDDing a leaderboard PORO

--- app/controllers/api/v1/leaderboards_controller.rb
+++ app/controllers/api/v1/leaderboards_controller.rb
@@ -14,14 +14,19 @@ class Api::V1::LeaderboardsController < ApplicationController
   def get_highscores
     case leaderboards_params[:type]
     when 'level_one'
+      @scores = Leaderboard.new(leaderboards_params[:type])
       @scores = User.order(level_one_fastest_time: :asc).limit(100)
     when 'level_two'
+      @scores = Leaderboard.new(leaderboards_params[:type])
       @scores = User.order(level_two_fastest_time: :asc).limit(100)
     when 'level_three'
+      @scores = Leaderboard.new(leaderboards_params[:type])
       @scores = User.order(level_three_fastest_time: :asc).limit(100)
     when 'level_four'
+      @scores = Leaderboard.new(leaderboards_params[:type])
       @scores = User.order(level_four_fastest_time: :asc).limit(100)
     when 'overall'
+      @scores = Leaderboard.new(leaderboards_params[:type])
       @scores = User.order(total_fastest_time: :asc).limit(100)
     else
       render json: {error: "Invalid type supplied."}, status: 422
