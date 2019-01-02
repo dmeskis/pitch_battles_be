5c9967e6e838f53448213119a12e5ce85533d939
Dylan Meskis <35268891+dmeskis@users.noreply.github.com>
2019-01-02 16:44:01 -0700

    Testing refactoring (#139)
    * Refactored multiple controllers and model methods

--- app/services/badge_analysis.rb
+++ app/services/badge_analysis.rb
@@ -15,73 +15,39 @@ class BadgeAnalysis
   def badge_earned?(badge)
     case badge.id
     when 1
-      if @user.games.count >= 5
-        @user.badges << badge
-      end
+      if @user.games.count >= 5 then @user.badges << badge end
     when 2
-      if @game.level_one_duration
-        @user.badges << badge
-      end
+      if @game.level_one_duration then @user.badges << badge end
     when 3
-      if @game.level_one_perfect
-        @user.badges << badge
-      end
+      if @game.level_one_perfect then @user.badges << badge end
     when 4
-      if @game.level_two_duration
-        @user.badges << badge
-      end
+      if @game.level_two_duration then @user.badges << badge end
     when 5
-      if @user.games.count >= 10
-        @user.badges << badge
-      end
+      if @user.games.count >= 10 then @user.badges << badge end
     when 6
-      if @game.level_two_perfect
-        @user.badges << badge
-      end
+      if @game.level_two_perfect then @user.badges << badge end
     when 7
-      if @game.level_three_duration
-        @user.badges << badge
-      end
+      if @game.level_three_duration then @user.badges << badge end
     when 8
-      if @user.games.count >= 20
-        @user.badges << badge
-      end
+      if @user.games.count >= 20 then @user.badges << badge end
     when 9
-      if @game.level_three_perfect
-        @user.badges << badge
-      end
+      if @game.level_three_perfect then @user.badges << badge end
     when 10
-      if @user.games.count >= 50
-        @user.badges << badge
-      end
+      if @user.games.count >= 50 then @user.badges << badge end
     when 11
-      if @game.level_four_duration
-        @user.badges << badge
-      end
+      if @game.level_four_duration then @user.badges << badge end
     when 12
-      if @game.level_four_perfect
-        @user.badges << badge
-      end
+      if @game.level_four_perfect then @user.badges << badge end
     when 13
-      if @user.games.count >= 100
-        @user.badges << badge
-      end
+      if @user.games.count >= 100 then @user.badges << badge end
     when 14
-      if @game.all_perfect
-        @user.badges << badge
-      end
+      if @game.all_perfect then @user.badges << badge end
     when 15
-      if @user.games.count >= 200
-        @user.badges << badge
-      end
+      if @user.games.count >= 200 then @user.badges << badge end
     when 16
-      if @user.games.count >= 500
-        @user.badges << badge
-      end
+      if @user.games.count >= 500 then @user.badges << badge end
     when 17
-      if @user.games.count >= 1000
-        @user.badges << badge
-      end
+      if @user.games.count >= 1000 then @user.badges << badge end
     end
   end
 
