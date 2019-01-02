1d5aaca6b00554348bba652246aba007abd56270
Mesky <dmeskis@gmail.com>
2019-01-02 14:56:00 -0700

    Create teacher dashboard controller, route, test

---
+++ app/controllers/api/v1/teacher_dashboard_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::TeacherDashboardController < ApplicationController
+  before_action :is_teacher?
+
+  def show
+    @klass = Klass.find_by(teacher_id: @current_user.id)
+    if @klass != nil
+      binding.pry
+      render json: TeacherDashboardSerializer.new(@klass).serialized_json
+    else
+      render json: {error: "No classes found" }, status: 404
+    end
+  end
+
+  private
+
+  def is_teacher?
+    unless @current_user.teacher?
+      render json: {"error": "You must be a teacher to create a class"}, status: 401
+    end
+  end
+
+end
---
+++ spec/api/v1/teacher_dashboard/teacher_dashboard_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+require 'helpers/login_helper'
+include LoginHelper
+
+describe 'teacher dashboard integration', :type => :request do
+  describe 'show' do
+    it 'gets teacher class data at the class dashboard endpoint' do
+      create_teacher
+      login
+      user = User.first
+
+      klass = create(:klass, teacher_id: user.id)
+      klass.users << user
+      10.times do
+        user = create(:user, klass_id: klass.id)
+        rand(0..10).times do
+          create(:game, user_id: user.id)
+        end
+        rand(0..10).times do
+          user.badges << create(:badge)
+        end
+      end
+
+
+      key = JSON.parse(response.body)["access_token"]
+
+      get "/api/v1/teacher_dashboard", :headers => {'AUTHORIZATION': "bearer #{key}"}
+      body = JSON.parse(response.body)
+
+      binding.pry
+      expect(body["data"]["attributes"]["most_badges"]["badges"]).to eq(klass.most_badges[:badges])
+      expect(body["data"]["attributes"]["most_games"]["total_games"]).to eq(klass.most_games[:total_games])
+    end
+  end
+end
