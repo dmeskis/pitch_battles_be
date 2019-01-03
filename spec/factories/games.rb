a871f8c1c94f2cb7b2b172556f17aafd08799de9
Mesky <dmeskis@gmail.com>
2019-01-03 14:36:04 -0700

    Update factory for games total duration to be sum of durations

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :game do
-    total_duration { Faker::Number.number(5) }
-    level_one_duration { Faker::Number.number(5) }
-    level_two_duration { Faker::Number.number(5) }
-    level_three_duration { Faker::Number.number(5) }
-    level_four_duration { Faker::Number.number(5) }
+    level_one_duration { Faker::Number.between(20000, 95000) }
+    level_two_duration { Faker::Number.between(30000, 110000) }
+    level_three_duration { Faker::Number.between(40000, 150000) }
+    level_four_duration { Faker::Number.between(50000, 200000) }
+    total_duration { level_one_duration + level_two_duration + level_three_duration + level_four_duration}
     user
   end
 end
