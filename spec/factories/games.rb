a6f02f1351a62958bb084857322bbd0921ec6bc9
Mesky <dmeskis@gmail.com>
2019-01-03 14:56:30 -0700

    Add perfect levels to games factory

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -4,6 +4,11 @@ FactoryBot.define do
     level_two_duration { Faker::Number.between(30000, 110000) }
     level_three_duration { Faker::Number.between(40000, 150000) }
     level_four_duration { Faker::Number.between(50000, 200000) }
+    level_one_perfect { [true, false].sample }
+    level_two_perfect { [true, false].sample }
+    level_three_pefect { [true, false].sample }
+    level_four_perfect { [true, false].sample }
+    all_perfect { [level_one_perfect, level_two_perfect, level_three_perfect, level_four_perfect].all? { |l| l == true} }
     total_duration { level_one_duration + level_two_duration + level_three_duration + level_four_duration}
     user
   end
