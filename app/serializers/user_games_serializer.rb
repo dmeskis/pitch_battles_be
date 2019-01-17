73a5974027bce3a90c620018f4303f106f5b15d0
Mesky <dmeskis@gmail.com>
2019-01-16 19:52:54 -0700

    refactoring

--- app/serializers/user_games_serializer.rb
+++ app/serializers/user_games_serializer.rb
@@ -2,9 +2,9 @@ class UserGamesSerializer
   include FastJsonapi::ObjectSerializer
   attributes :email, :first_name, :last_name
   set_type :user
-  attribute :games do |obj|
-    Rails.cache.fetch(obj.game_cache_key(obj.games)) do
-      GameSerializer.new(Game.where(user_id: obj.id))
+  attribute :games do |user|
+    Rails.cache.fetch(user.game_cache_key(user.games)) do
+      GameSerializer.new(user.games)
     end
   end
 
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,17 +1,17 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :email, :first_name, :last_name, :avatar, :role, :level_one_fastest_time, :level_two_fastest_time, :level_three_fastest_time, :level_four_fastest_time, :total_games_played
-  attribute :games do |obj|
-    Rails.cache.fetch(obj.game_cache_key(obj.games)) do
-      GameSerializer.new(Game.where(user_id: obj.id))
+  attribute :games do |user|
+    Rails.cache.fetch(user.game_cache_key(user.games)) do
+      GameSerializer.new(user.games)
     end
   end
-  attribute :badges do |obj|
-    Rails.cache.fetch(obj.badge_cache_key(obj.badges)) do
-      BadgeSerializer.new(obj.badges)
+  attribute :badges do |user|
+    Rails.cache.fetch(user.badge_cache_key(user.badges)) do
+      BadgeSerializer.new(user.badges)
     end
   end
-  attribute :class do |obj|
-    KlassSerializer.new(obj.klass)
+  attribute :class do |user|
+    KlassSerializer.new(user.klass)
   end
 end
