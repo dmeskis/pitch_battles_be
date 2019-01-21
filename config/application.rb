b176d0d7814f2b9725deaae820d80b8dfe98bca8
Mesky <dmeskis@gmail.com>
2019-01-21 15:57:21 -0700

    Allow CORS globally

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module PitchBattlesBe
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://pitchbattles.herokuapp.com'
+        origins '*'
         resource '*', headers: :any, methods: [:get, :post, :patch, :delete, :options]
       end
     end
